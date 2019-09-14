c5f580f587a7d613251f3de5873c91a60bb64e35
marinolondono <marino.londono@arkix.com>
2019-09-14 14:50:57 -0500

    aplicación eventos event brite context-react

---
+++ src/context/EventosContext.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+const EventosContext = React.createContext();
+export const EventosConsumer = EventosContext.Consumer;
+
+class EventosProvider extends Component {
+    token = '5FQKPL6LYMAOHEZHK662';
+    ordenar = 'date';
+    state = {
+        eventos:[]
+    }
+    obtenerEventos = async (busqueda) => {
+        let url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}&categories=${busqueda.categoria}&sort_by=${this.ordenar}&token=${this.token}&locale=es_ES`;
+        // consultar la API con la url
+        const eventos = await axios(url);
+        this.setState({
+            eventos:eventos.data.events
+        })
+    }
+    render() {
+       
+        return (
+            <EventosContext.Provider
+                value={{
+                    eventos: this.state.eventos,
+                    obtenerEventos: this.obtenerEventos
+            }}>
+                {this.props.children}
+            </EventosContext.Provider>
+        );
+    }
+}
+
+export default EventosProvider;
